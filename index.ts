d9ed3b2fa67dd3735868e6e2468d1f7687238e1e
Antony Montalvo <antony.montalvo@mediabyte.com.pe>
2021-06-13 14:01:13 -0500

    Finish

--- index.ts
+++ index.ts
@@ -1,6 +1,20 @@
+function reverse(x: number): number {
+  const limit = 2147483648;
+  const factor = x < 0 ? -1 : 1;
+  if(factor < 0 ? x < limit*factor : x > limit-1) return 0
+  x = factor < 0 ? -x : x
+  x = Number(x.toString().split('').reverse().join(''));
+  x = factor < 0 ? x*factor : x
+  if(factor < 0 ? x < limit*factor : x > limit-1) return 0
+  
+  return x;
+};
 
 function main(){
+  let num = -2147483648;
+  console.log(reverse(num));
   
+  return reverse(num)
 }
 
 main();
