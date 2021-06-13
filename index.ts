021ae157f48165939705af51040dfb11591dff8d
Antony Montalvo <antony.montalvo@mediabyte.com.pe>
2021-06-13 16:49:37 -0500

    Finish

--- index.ts
+++ index.ts
@@ -1,3 +1,11 @@
+function isPalindrome(x: number): boolean {
+    const negative =  x < 0 ? -1 : 1;
+    if(negative == -1) return false;
+
+    let reverse = Number(x.toString().split('').reverse().join(''))
+
+    return x == reverse;
+};
 
 function main(){
   
