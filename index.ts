56d32dc38da222cfb3293f3b91724614d46223d2
Antony Montalvo <antony.montalvo@mediabyte.com.pe>
2021-06-13 23:11:36 -0500

    Finish

--- index.ts
+++ index.ts
@@ -1,6 +1,20 @@
+function longestCommonPrefix(strs: string[]): string {
+    let continueFor = true;
+    let prefix = [];
+    
+    for (let i = 0; i < strs[0].length && continueFor; i++) {
+        let similar = strs[0].split('')[i];
+        let matches = strs.filter(s => similar == s.split('')[i])
+        if(strs.length != matches.length) continueFor = false;
+        else prefix.push(similar)
+    }
+    return prefix.join('')
+};
 
 function main(){
-  
+  let strs = ["flower","flow","flight"]
+  console.log(longestCommonPrefix(strs))
+  return longestCommonPrefix(strs)
 }
 
 main();
