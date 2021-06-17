9bac913ea90d29ae51dbabc49248881dcbb55926
Antony Montalvo <antonis162010@gmail.com>
2021-06-16 23:56:39 -0500

    Finish

--- index.ts
+++ index.ts
@@ -1,6 +1,35 @@
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
 
-function main(){
-  
+function mergeTwoLists(
+  l1: ListNode | null,
+  l2: ListNode | null
+): ListNode | null {
+  if (l1 == null) return l2;
+  if (l2 == null) return l1;
+
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoLists(l2.next, l1);
+    return l2;
+  }
+}
+
+function main() {
+  let l1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+  let l2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+  console.log(mergeTwoLists(l1, l2));
+
+  return mergeTwoLists(l1, l2);
 }
 
 main();
