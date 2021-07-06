d29997be44bcbd9d88bd4b4558cfe0c449262f0c
Antony Montalvo <antonis162010@gmail.com>
2021-07-06 11:48:35 -0500

    Codility test

--- index.ts
+++ index.ts
@@ -1,6 +1,24 @@
+function solution(A) {
+  // write your code in JavaScript (Node.js 8.9.4)
+  let existNegative = A.findIndex((a) => a < 0) > -1 ? true : false;
+  let resp = null;
+  if (!existNegative) {
+    let continueS = true;
+    let i;
+    for (i = 0; i < A.length && continueS; i++) {
+      if (A.findIndex((a) => a == i + 1) == -1) {
+        resp = i + 1;
+        continueS = false;
+      }
+    }
 
-function main(){
-  
+    if (!resp) resp = i + 1;
+  } else resp = 1;
+  return resp;
+}
+
+function main() {
+  console.log(solution([1, 3, 2]));
 }
 
 main();
